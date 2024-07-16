e46392290a997097d5e5809829570e79b0b4cfeb
Jae Jeong <jeongjj29@gmail.com>
2024-07-16 17:37:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (names) {
+        return names.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, letters) {
+    const lowercaseLetters = letters.toLowerCase();
+    return drivers.filter(function (driver) {
+        const lowercaseDriver = driver.toLowerCase();
+        return lowercaseDriver.startsWith(lowercaseLetters);
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.name === name;
+    })
+}
